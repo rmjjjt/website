15fe1aff65639f04237a140a3fe33b68d8d0e960
Rhymes Toasterface <rhyme_master@hotmail.co.uk>
2017-01-10 11:49:22 +0000

    Fixed Issue With Inclueds Nav File.
    All links were generating notices as variables weren't set.
    Defined as empty strings unless set - the variabe is set on the specific page to add a class to the nav link.

--- includes/nav.php
+++ includes/nav.php
@@ -1,4 +1,24 @@
 <?php
+if(empty($home)) {
+    $home = '';
+}
+
+if(empty($portfolio)) {
+    $portfolio = '';
+}
+
+if(empty($contact)) {
+    $contact = '';
+}
+
+if(empty($about)) {
+    $about = '';
+}
+
+if(empty($blog)) {
+    $blog = '';
+}
+
 echo '<nav>' .
     '<a class="navLink ' . $home . '" href="index.php">home</a>' .
     '<a class="navLink ' . $portfolio . '" href="portfolio.php">portfolio</a>' .
